7f32110380150b4530e83f3b5cbcbd2681ce654d
Nick Strong <nstrong92@yahoo.com>
2021-04-22 07:42:54 -0600

    added a set of variables for the available characters that can be generated

--- Develop/script.js
+++ Develop/script.js
@@ -1,13 +1,33 @@
+// Original code commented out to have start point refrence
+
+// // Assignment Code
+// var generateBtn = document.querySelector("#generate");
+
+// // Write password to the #password input
+// function writePassword() {
+//   var password = generatePassword();
+//   var passwordText = document.querySelector("#password");
+
+//   passwordText.value = password;
+
+// }
+
+// // Add event listener to generate button
+// generateBtn.addEventListener("click", writePassword);
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var abdUpper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var abdLower = "abcdefghijklmnopqrstuvwxyz";
+var num = "0123456789";
+var symbols = "~`!@#$%^&*()_-+=[]{}|/?><,.";
 
 // Write password to the #password input
-function writePassword() {
+function writePassword(password) {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
